7017302967ba08ed8a28932d27d8ab1de689b684
Ditendek <alex2003@gmx.net>
2026-09-01 09:19:18 +0200

    quicksave...

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,42 +1,78 @@
 return {
-  'stevearc/conform.nvim',
-  event = { 'BufWritePre' },
-  cmd = { 'ConformInfo' },
-  keys = {
-    {
-      -- Customize or remove this keymap to your liking
-      '<leader>f',
-      function() require('conform').format { async = true } end,
-      mode = '',
-      desc = 'Format buffer',
+    'stevearc/conform.nvim',
+    event = { 'BufWritePre' },
+    cmd = { 'ConformInfo' },
+    keys = {
+        {
+            -- Customize or remove this keymap to your liking
+            '<leader>f',
+            function() require('conform').format { async = true } end,
+            mode = '',
+            desc = 'Format buffer',
+        },
     },
-  },
-  -- This will provide type hinting with LuaLS
-  ---@module "conform"
-  ---@type conform.setupOpts
-  opts = {
-    -- Define your formatters
-    formatters_by_ft = {
-      lua = { 'stylua' },
-      python = { 'isort', 'black' },
-      javascript = { 'prettierd', 'prettier', stop_after_first = true },
-      c = { 'clang-format' },
+    -- This will provide type hinting with LuaLS
+    ---@module "conform"
+    ---@type conform.setupOpts
+    opts = {
+        -- Define your formatters
+        formatters_by_ft = {
+            lua = { 'stylua' },
+            python = { 'isort', 'black' },
+            javascript = { 'prettierd', 'prettier', stop_after_first = true },
+            c = { 'clang-format' },
+            cpp = { 'clang-format' },
+        },
+        -- Set default options
+        default_format_opts = {
+            lsp_format = 'fallback',
+        },
+        -- Set up format-on-save
+        format_on_save = { timeout_ms = 500 },
+        -- Customize formatters
+        formatters = {
+            shfmt = {
+                append_args = { '-i', '2' },
+            },
+            stylua = {
+                prepend_args = {
+                    '--indent-type',
+                    'Spaces',
+                    '--indent-width',
+                    '4',
+                },
+            },
+
+            black = {
+                prepend_args = {
+                    '--line-length',
+                    '88',
+                },
+            },
+
+            prettier = {
+                prepend_args = {
+                    '--tab-width',
+                    '4',
+                },
+            },
+
+            prettierd = {
+                prepend_args = {
+                    '--tab-width',
+                    '4',
+                },
+            },
+
+            ['clang-format'] = {
+                prepend_args = {
+                    '--style={BasedOnStyle: LLVM, IndentWidth: 4, TabWidth: 4, UseTab: Never, AllowShortFunctionsOnASingleLine: None}',
+                },
+            },
+        },
     },
-    -- Set default options
-    default_format_opts = {
-      lsp_format = 'fallback',
-    },
-    -- Set up format-on-save
-    format_on_save = { timeout_ms = 500 },
-    -- Customize formatters
-    formatters = {
-      shfmt = {
-        append_args = { '-i', '2' },
-      },
-    },
-  },
-  init = function()
-    -- If you want the formatexpr, here is the place to set it
-    vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-  end,
+    init = function()
+        -- If you want the formatexpr, here is the place to set it
+        vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+    end,
 }
